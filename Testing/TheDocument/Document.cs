069475a308fffc4d589005256de8014a891fdc98
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-05-16 21:12:16 +0200

    Documentos

---
+++ Testing/TheDocument/Document.cs
@@ -0,0 +1,67 @@
+namespace TheDocument;
+
+public class Document
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public List<Chapter> Chapters { get; set; }
+    public bool IsPublished { get; private set; }
+
+    public void Publish()
+    {
+        if (string.IsNullOrEmpty(Author))
+            throw new AuthorMustBeProvidedException();
+        if (string.IsNullOrEmpty(Title))
+            throw new TitleMustBeProvidedException();
+        if (Chapters.Count < 3)
+            throw new AtLeastThreeChaptersMustBeProvidedException();
+
+        foreach (var chapter in Chapters)
+        {
+            if (string.IsNullOrEmpty(chapter.Title))
+                throw new ChapterTitleMustBeProvidedException();
+            if (string.IsNullOrEmpty(chapter.Content))
+                throw new ChapterContentMustBeProvidedException();
+            if (chapter.Content.Split(' ').Length < 10)
+                throw new ChapterContentMustContainAtLeastTenWordsException();
+        }
+
+        IsPublished = true;
+    }
+}
+
+public class ChapterContentMustContainAtLeastTenWordsException : Exception
+{
+}
+
+public class AtLeastThreeChaptersMustBeProvidedException : Exception
+{
+}
+
+public class ChapterContentMustBeProvidedException : Exception
+{
+}
+
+public class ChapterTitleMustBeProvidedException : Exception
+{
+}
+
+public class AuthorMustBeProvidedException : Exception
+{
+}
+
+public class TitleMustBeProvidedException : Exception
+{
+}
+
+public class Chapter
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
+
+public class Section
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
---
+++ Testing/TheDocument/UnitTest1.cs
@@ -0,0 +1,60 @@
+namespace TheDocument;
+
+public class UnitTest
+{
+    [Fact]
+    public void Publish_WithLessThanThreeChapters_ThrowsException()
+    {
+        var document = new Document
+        {
+            Author = "Author",
+            Title = "Title",
+            Chapters = new List<Chapter>
+            {
+                new Chapter { Title = "Chapter 1", Content = "Some content here" },
+                new Chapter { Title = "Chapter 2", Content = "Some other content here" }
+            }
+        };
+
+        Assert.Throws<AtLeastThreeChaptersMustBeProvidedException>(() => document.Publish());
+    }
+
+    [Fact]
+    public void Publish_WithChapterHavingLessThanTenWords_ThrowsException()
+    {
+        var document = new Document
+        {
+            Author = "Author",
+            Title = "Title",
+            Chapters = new List<Chapter>
+            {
+                new Chapter { Title = "Chapter 1", Content = "Few words" },
+                new Chapter { Title = "Chapter 2", Content = "Some other content here" },
+                new Chapter { Title = "Chapter 3", Content = "Additional content here" }
+            }
+        };
+
+        Assert.Throws<ChapterContentMustContainAtLeastTenWordsException>(() => document.Publish());
+    }
+
+    [Fact]
+    public void Publish_WithValidDocument_SetsIsPublishedToTrue()
+    {
+        var document = new Document
+        {
+            Author = "Author",
+            Title = "Title",
+            Chapters = new List<Chapter>
+            {
+                new Chapter { Title = "Chapter 1", Content = "Some content here with more than ten words" },
+                new Chapter { Title = "Chapter 2", Content = "Some other content here with more than ten words" },
+                new Chapter { Title = "Chapter 3", Content = "Additional content here with more than ten words" }
+            }
+        };
+
+        document.Publish();
+
+        Assert.True(document.IsPublished);
+    }
+
+}
