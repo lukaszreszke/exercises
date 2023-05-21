b052ed656fb097060ae1cd45ce6b14e64312b0a2
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-05-21 18:45:00 +0200

    Uniq email per favore

---
+++ Testing/UniqueEmail/User.cs
@@ -0,0 +1,27 @@
+﻿namespace UniqueEmail;
+
+public class User
+{
+    public User(string email, IUserRepository userRepository)
+    {
+        var user = userRepository.FindByEmail(email);
+        if (user != null)
+            throw new UserEmailNotAvailable(email);
+    }
+}
+
+public class UserEmailNotAvailable : Exception
+{
+    public string Email { get; }
+
+    public UserEmailNotAvailable(string email)
+    {
+        Email = email;
+    }
+}
+
+
+public interface IUserRepository
+{
+    User FindByEmail(string email);
+}
