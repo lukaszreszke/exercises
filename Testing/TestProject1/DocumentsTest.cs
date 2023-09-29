e18874c7fa7043a159ff3e0813546c33f3602310
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-09-29 06:41:25 +0200

    Add unit tests for document
    Preparing for refactoring. Those are the scenarios that I am starting
    with

---
+++ Testing/TestProject1/DocumentsTest.cs
@@ -0,0 +1,34 @@
+namespace TestProject1;
+
+public class DocumentsTest
+{
+    [Fact]
+    public void draft_is_default_status()
+    {
+        
+    }
+
+    [Fact]
+    public void verify_draft_document()
+    {
+        
+    }
+
+    [Fact]
+    public void publish_verified_document()
+    {
+        
+    }
+
+    [Fact]
+    public void cannot_publish_non_verified_document()
+    {
+        
+    }
+
+    [Fact]
+    public void cannot_verify_non_draft_document()
+    {
+        
+    }
+}
