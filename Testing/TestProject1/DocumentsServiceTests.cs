0ce6398fa47e37a652fffe84efe18e26ad3546c4
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-09-29 06:50:48 +0200

    Publish document failing test
    It doesn't even compile

--- Testing/TestProject1/DocumentsServiceTests.cs
+++ Testing/TestProject1/DocumentsServiceTests.cs
@@ -61,4 +61,35 @@ public class DocumentsServiceTests
         emailGatewayMock.Verify(x => x.SendEmail(user.Email, 
             $"Document Tests has been verified by {user.Id}"));
     }
+    
+    [Fact]
+    public void publishes_document()
+    {
+        var documentsRepository = new InMemoryDocumentRepository();
+        var usersRepository = new InMemoryUsersRepository();
+        var contextAccessorMock = new Mock<IExecutionContextAccessor>();
+        var emailGatewayMock = new Mock<IEmailGateway>();
+        var user = new User(Guid.NewGuid()) { Email = "tests@possible.com" };
+        usersRepository.Save(user);
+        contextAccessorMock.Setup(x => x.UserId).Returns(user.Id);
+        var documentsService = new DocumentsService(
+            documentsRepository,
+            contextAccessorMock.Object,
+            null,
+            emailGatewayMock.Object,
+            usersRepository);
+        var documentId = documentsService.CreateDocument(DocumentType.MANUAL, "Tests",
+            "Are Cool And Possible To Do In Your Code. Check How.");
+        documentsService.VerifyDocument(documentId);
+ 
+        documentsService.PublishDocument(documentId);
+
+        var result = documentsRepository.GetById(documentId);
+        Assert.Equal("PUBLISHED", result.Status.Code);
+        emailGatewayMock.Verify(x => x.SendEmail(user.Email, 
+            "Document Tests is printed and waits for you in the office." +
+            "You have to come and get it personally." +
+            "Your manager," +
+            "Control Desire", time));
+    }
 }
