05a22fe62e35b5ddf3284cc1ce21d609cdad153f
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-12-19 07:32:57 +0100

    Better

--- Testing/FlightReservationAssertObjectTests/FlightReservationAssertObject.cs
+++ Testing/FlightReservationAssertObjectTests/FlightReservationAssertObject.cs
@@ -4,12 +4,28 @@ namespace FlightReservationAssertObjectTests;
 
 public static class FlightReservationAssertObject
 {
-    public static void AssertFlightReservation(this FlightReservation actualReservation, int expectedReservationId, string expectedPassengerName, string expectedFlightNumber, DateTime expectedDepartureTime, bool expectedIsConfirmed)
+    public static void HasCorrectReservationId(this FlightReservation actual, int reservationId)
     {
-        Assert.Equal(expectedReservationId, actualReservation.ReservationId);
-        Assert.Equal(expectedPassengerName, actualReservation.PassengerName);
-        Assert.Equal(expectedFlightNumber, actualReservation.FlightNumber);
-        Assert.Equal(expectedDepartureTime, actualReservation.DepartureTime);
-        Assert.Equal(expectedIsConfirmed, actualReservation.IsConfirmed);
+        Assert.Equal(reservationId, actual.ReservationId);
+    }
+    
+    public static void HasCorrectPassengerName(this FlightReservation actual, string passengerName)
+    {
+        Assert.Equal(passengerName, actual.PassengerName);
+    }
+    
+    public static void HasCorrectFlightNumber(this FlightReservation actual, string flightNumber)
+    {
+        Assert.Equal(flightNumber, actual.FlightNumber);
+    }
+    
+    public static void HasCorrectDepartureTime(this FlightReservation actual, DateTime departureTime)
+    {
+        Assert.Equal(departureTime, actual.DepartureTime);
+    }
+    
+    public static void HasCorrectConfirmed(this FlightReservation actual,bool confirmed)
+    {
+        Assert.Equal(confirmed, actual.IsConfirmed);
     }
 }
