39f2d2dc93a02376470b64a589482de27b71dc59
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-04-03 06:12:44 +0200

    Tribute to Blazor

---
+++ Testing/SalaryCalculator/SalaryFormObject.cs
@@ -0,0 +1,8 @@
+namespace SalaryCalculator;
+
+public class SalaryFormObject
+{
+    public string Period { get; set; }
+    public decimal BaseAmount { get; set; }
+    public decimal Result { get; set; }
+}
